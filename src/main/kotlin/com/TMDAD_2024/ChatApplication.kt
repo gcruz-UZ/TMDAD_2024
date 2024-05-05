da31d2fd7572ae74c8545de2be62840ee20d846a
Guillermo Cruz <gcruz@etiqmedia.com>
2024-05-05 16:43:20 +0200

    Añadido ejemplo sencillo commandLineRunner (thread printea en consola cada 30 segs)

--- src/main/kotlin/com/TMDAD_2024/ChatApplication.kt
+++ src/main/kotlin/com/TMDAD_2024/ChatApplication.kt
@@ -3,6 +3,24 @@ package com.TMDAD_2024
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+@Component
+class ConsoleMessagePrinter : CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+		val scheduler = Executors.newSingleThreadScheduledExecutor()
+		val intervalInSeconds = 30L
+
+		scheduler.scheduleAtFixedRate({
+			println("Printing a message every $intervalInSeconds seconds")
+		}, 0, intervalInSeconds, TimeUnit.SECONDS)
+	}
+}
+
 @SpringBootApplication
 class ChatApplication
 
