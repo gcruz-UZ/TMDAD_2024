d025569745ef848b7f04f1d6aa651b989f3970ff
gcruz-UZ <682433@unizar.es>
2024-05-16 13:42:06 +0200

    Deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	application
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.23"
