ea2042f3caab88d293a94e9a4b995b573760f8c0
gcruz-UZ <682433@unizar.es>
2024-05-16 12:03:41 +0200

    Deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	application
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.23"
@@ -50,7 +51,11 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-springBoot { mainClass.set("com.TMDAD_2024.ChatApplicationKt") }
+//springBoot { mainClass.set("com.TMDAD_2024.ChatApplicationKt") }
+
+application {
+	mainClass = "com.TMDAD_2024.ChatApplication"
+}
 
 tasks.withType<Jar> {
 	manifest {
